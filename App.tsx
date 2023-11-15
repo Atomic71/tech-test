e501e36eab9aeafd24449e6c47890b5b8155f931
Atomic71 <tomicandrej71@gmail.com>
2023-11-15 23:58:01 +0100

    render everything in main app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Counter from './components/Counter';
+import LoginForm from './components/LoginForm';
+import { UserListItem } from './components/User';
+import useUsers from './hooks/useUsers';
 
 export default function App() {
+  const users = useUsers();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <View style={styles.screenContainer}>
+        <StatusBar style='auto' />
+        <Text style={styles.title}>Excercise #1</Text>
+        <ScrollView>
+          {users.length > 0 &&
+            users.map((user, i) => (
+              <UserListItem
+                user={user}
+                key={user.id}
+              />
+            ))}
+        </ScrollView>
+
+        <Text style={styles.title}>Excercise #2:</Text>
+        <Counter></Counter>
+        <Text style={styles.title}>Excercise #3:</Text>
+        <LoginForm></LoginForm>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  screenContainer: {
+    paddingHorizontal: 20,
+    paddingTop: 30,
+    flex: 1,
+    gap: 20,
+  },
+  title: {
+    marginBottom: 10,
+    fontSize: 30,
   },
 });
